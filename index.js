46abfc14e3b85cf500be9cb06bc7060b9fe5fa47
Hollis Crowder <hollis.crowder@gmail.com>
2024-03-12 10:30:24 -0700

    complete lab again during phase 1

--- index.js
+++ index.js
@@ -3,44 +3,40 @@ const cats = ["Milo", "Otis", "Garfield"];
 
 // destructively append a cat to the end of cats
 function destructivelyAppendCat(name) {
-    cats.push(name);
-};
+  return cats.push(name);
+}
 
 // destructively prepend a cat to the beginning of cats
 function destructivelyPrependCat(name) {
-    cats.unshift(name);
-};
+  return cats.unshift(name);
+}
 
 // destructively remove the last cat from the end of cats
-function destructivelyRemoveLastCat(name) {
-    cats.pop(name);
-};
+function destructivelyRemoveLastCat() {
+  return cats.pop();
+}
 
 // destructively remove the first cat from the beginning of cats
-function destructivelyRemoveFirstCat(name) {
-    cats.shift(name);
-};
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
 
 // non-destructively adds a cat to the end of cats
 function appendCat(name) {
-    const newCats = [...cats, name];
-    return newCats;
-};
+  return [...cats, name];
+}
 
 // non-destructively prepends a cat to the beginning of cats
 function prependCat(name) {
-    const newCats = [name, ...cats];
-    return newCats;
-};
+  return [name, ...cats];
+}
 
 // non-destructively removes the last cat from cats
-function removeLastCat(name) {
-    const newCats = cats.slice(0,-1);
-    return newCats;
-};
+function removeLastCat() {
+  return cats.slice(0, cats.length - 1);
+}
 
 // non-destructively remove the first cat from cats
-function removeFirstCat(name) {
-    const newCats = cats.slice(1);
-    return newCats;
-};
+function removeFirstCat() {
+  return cats.slice(1);
+}
